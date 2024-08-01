5342a4536728e01fe3cd4f1310bc047846883f5f
Daniel Giao <daniel.giao@icloud.com>
2024-07-31 21:06:38 -0400

    Added workout schema and model

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,30 +1,36 @@
 const express = require('express')
-
+const Workout = require('../models/workoutModel')
 const router = express.Router()
 
 // GET all workouts
-router.get('/', (req,res) => {
-    res.json({msg: 'GET all workouts'})
+router.get('/', (req, res) => {
+    res.json({ msg: 'GET all workouts' })
 })
 
 // GET a single workouts
-router.get('/:id', (req,res) => {
-    res.json({msg: 'GET a single workout'})
+router.get('/:id', (req, res) => {
+    res.json({ msg: 'GET a single workout' })
 })
 
 // POST a new workout
-router.post('/', (req,res)=>{
-    res.json({msg: 'POST a new workout'})
+router.post('/', async (req, res) => {
+    const { title, reps, load } = req.body
+    try {
+        const workout = await Workout.create({ title, reps, load })
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
 // DELETE workout
-router.delete('/:id', (req,res)=>{
-    res.json({msg: 'DELETE a workout'})
+router.delete('/:id', (req, res) => {
+    res.json({ msg: 'DELETE a workout' })
 })
 
 // UPDATE workout
-router.patch('/:id', (req,res)=>{
-    res.json({msg: 'UPDATE a workout'})
+router.patch('/:id', (req, res) => {
+    res.json({ msg: 'UPDATE a workout' })
 })
 
 module.exports = router
