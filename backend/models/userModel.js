55429bdd00c21903ecbc51c41d3de56ed1e3bfa2
Daniel Giao <daniel.giao@icloud.com>
2024-08-05 00:06:08 -0400

    Added email and password validation via validator pkg

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
+
+// libraries import
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -17,6 +20,17 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+    // validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
     const exist = await this.findOne({ email })
 
     if (exist) {
