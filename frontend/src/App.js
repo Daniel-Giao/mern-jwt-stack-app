c047c2a7bc90426d841e681c2f8e465054edcbc3
Daniel Giao <daniel.giao@icloud.com>
2024-08-10 18:42:24 -0400

    Protected React routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 // rrd imports
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// hooks import
+import useAuthContext from "./hooks/useAuthContext";
 
 // components import
 import NavBar from './components/Navbar'
@@ -10,6 +13,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 function App() {
+  const { user } = useAuthContext()
   return (
     <div className="App">
       <BrowserRouter>
@@ -18,15 +22,15 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={<Home />}
+              element={user ? <Home /> : <Navigate to="/login" />}
             />
             <Route
               path="/login"
-              element={<Login />}
+              element={!user ? <Login /> : <Navigate to="/" />}
             />
             <Route
               path="/signup"
-              element={<Signup />}
+              element={!user ? <Signup /> : <Navigate to="/" />}
             />
           </Routes>
         </div>
